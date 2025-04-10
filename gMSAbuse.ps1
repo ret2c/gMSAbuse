8c4210090edddb0b2139a48c1c012f1e4a890749
connor <connorskastner@gmail.com>
2025-04-10 17:01:04 -0500

    gMSAbuse Creation

---
+++ gMSAbuse.ps1
@@ -0,0 +1,156 @@
+# gMSAAbuse.ps1
+# Script to retrieve all gMSA(s) on domain and check for abuse potential against current user
+
+Import-Module ActiveDirectory
+
+$CU = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$CurrentUserName = $CU.Name
+Write-Host "[+] Script running as: $CurrentUserName"
+
+try {
+    $adUser = Get-ADUser -Identity ($CurrentUserName.Split('\')[1]) -Properties MemberOf
+    $userGroups = @()
+    foreach ($group in $adUser.MemberOf) {
+        try {
+            $groupObj = Get-ADObject $group -Properties Name
+            $userGroups += $groupObj.Name
+        } catch {
+            $userGroups += $group.ToString()
+        }
+    }
+    Write-Host "[+] $CurrentUserName is a member of the following groups:"
+    $userGroups | ForEach-Object { Write-Host "  - $_" }
+} catch {
+    Write-Host "[!] Could not retrieve group memberships for current user $_" -ForegroundColor Red
+}
+
+Write-Host "[+] Starting gMSA enumeration"
+
+$gmsaAccounts = Get-ADServiceAccount -Filter * -Properties memberOf
+$results = @()
+
+foreach ($account in $gmsaAccounts) {
+    $memberOf = $account.memberOf
+    
+    if ($null -eq $memberOf -or $memberOf.Count -eq 0) {
+        continue
+    }
+    
+    $groupNames = @()
+    foreach ($group in $memberOf) {
+        try {
+            $obj = Get-ADObject $group -Properties Name
+            $groupNames += $obj.Name
+        } catch {
+            $groupNames += $group.ToString()
+        }
+    }
+    
+    $accountInfo = [PSCustomObject]@{
+        Name = $account.Name
+        SamAccountName = $account.SamAccountName
+        DistinguishedName = $account.DistinguishedName
+        Enabled = $account.Enabled
+        MemberOf = ($groupNames -join ", ")
+    }
+    
+    $results += $accountInfo
+}
+
+$results | ForEach-Object {
+    Write-Host "Name: " -NoNewline
+    Write-Host $_.Name
+    
+    Write-Host "SamAccountName: " -NoNewline
+    Write-Host $_.SamAccountName
+    
+    Write-Host "DistinguishedName: " -NoNewline
+    Write-Host $_.DistinguishedName
+    
+    Write-Host "Enabled: " -NoNewline
+    if ($_.Enabled -eq $true) {
+        Write-Host "True" -ForegroundColor Green
+    } else {
+        Write-Host "False" -ForegroundColor Red
+    }
+    
+    Write-Host "MemberOf: " -NoNewline
+    Write-Host $_.MemberOf
+    
+    Write-Host ""
+}
+
+if ($results.Count -gt 0) {
+    Write-Host "[+] Found $($results.Count) gMSA(s) with group memberships" -ForegroundColor Green
+} else {
+    Write-Host "[!] No gMSA(s) with group memberships found" -ForegroundColor Red
+    exit
+}
+
+Write-Host "[+] Checking for permissions on gMSA(s)"
+
+$schemaIDGUID = @{}
+Get-ADObject -SearchBase (Get-ADRootDSE).schemaNamingContext -LDAPFilter '(name=ms-ds-GroupMSAMembership)' -Properties name, schemaIDGUID |
+ForEach-Object {$schemaIDGUID.add([System.GUID]$_.schemaIDGUID,$_.name)}
+
+$userIdentities = @($CurrentUserName)
+foreach ($group in $userGroups) {
+    $userIdentities += $group
+    $userIdentities += "$((Get-ADDomain).NetBIOSName)\$group"
+}
+
+Write-Host "[+] Checking for permissions that allow modification of gMSA attributes"
+$vulnerableGMSAs = @()
+
+Set-Location ad:
+foreach ($gmsa in $results) {
+    $hasPermission = $false
+    $permissionDetails = @()
+    
+    $acl = Get-Acl $gmsa.DistinguishedName
+    $relevantAces = $acl.Access | Where-Object { 
+        (($_.AccessControlType -eq 'Allow') -and 
+         ($_.activedirectoryrights -in ('GenericAll') -and 
+          $_.inheritancetype -in ('All', 'None'))) -or 
+        (($_.activedirectoryrights -like '*WriteProperty*') -and 
+         ($_.objecttype -eq '00000000-0000-0000-0000-000000000000'))
+    }
+    
+    foreach ($ace in $relevantAces) {
+        $identity = $ace.IdentityReference.ToString()
+        if ($userIdentities -contains $identity) {
+            $hasPermission = $true
+            $permissionDetails += "- $identity has $($ace.ActiveDirectoryRights) rights"
+        }
+    }
+    
+    $specificAces = $acl.Access | Where-Object {
+        (($_.AccessControlType -eq 'Allow') -and 
+         ($_.activedirectoryrights -like '*WriteProperty*') -and 
+         ($_.objecttype -in $schemaIDGUID.Keys))
+    }
+    
+    foreach ($ace in $specificAces) {
+        $identity = $ace.IdentityReference.ToString()
+        if ($userIdentities -contains $identity) {
+            $hasPermission = $true
+            $permissionDetails += "- $identity has $($ace.ActiveDirectoryRights) rights on ms-ds-GroupMSAMembership attribute"
+        }
+    }
+    
+    if ($hasPermission) {
+        $vulnerableGMSAs += $gmsa
+        Write-Host "`n[!] VULNERABLE: Current user can modify attributes on gMSA: $($gmsa.Name)" -ForegroundColor Red
+        foreach ($detail in $permissionDetails) {
+            Write-Host "  $detail" -ForegroundColor Yellow
+        }
+    }
+}
+
+Set-Location $env:USERPROFILE
+
+if ($vulnerableGMSAs.Count -gt 0) {
+    Write-Host "`n[!] Found $($vulnerableGMSAs.Count) gMSA(s) that the current user can potentially abuse!" -ForegroundColor Red
+} else {
+    Write-Host "`n[+] No gMSA(s) found that the current user can modify." -ForegroundColor Green
+}
